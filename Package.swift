0737e9c584c605d8190cc9db02084454490902ca
Арман Чархчян <re-mont1@mail.ru>
2022-05-02 17:05:48 +0400

    fix

--- Package.swift
+++ Package.swift
@@ -3,6 +3,29 @@
 
 import PackageDescription
 
+private let remoteDependencies: [Package.Dependency] = [
+    .package(url: "https://github.com/Swinject/Swinject.git", from: "2.8.0"),
+    .package(url: "https://github.com/arman095095/Managers.git", branch: "develop"),
+    .package(url: "https://github.com/arman095095/Module.git", branch: "develop"),
+    .package(url: "https://github.com/arman095095/DesignSystem.git", branch: "develop"),
+    .package(url: "https://github.com/arman095095/AlertManager.git", branch: "develop"),
+    .package(url: "https://github.com/arman095095/Account.git", branch: "develop"),
+    .package(url: "https://github.com/arman095095/Profile.git", branch: "develop")
+]
+
+private let localDependencies: [Package.Dependency] = [
+    .package(url: "https://github.com/Swinject/Swinject.git", from: "2.8.0"),
+    .package(path: "/Users/armancarhcan/Desktop/Workdir/Managers"),
+    .package(path: "/Users/armancarhcan/Desktop/Workdir/Module"),
+    .package(path: "/Users/armancarhcan/Desktop/Workdir/DesignSystem"),
+    .package(path: "/Users/armancarhcan/Desktop/Workdir/AlertManager"),
+    .package(path: "/Users/armancarhcan/Desktop/Workdir/Account"),
+    .package(path: "/Users/armancarhcan/Desktop/Workdir/Profile")
+]
+
+let isDev = true
+private let dependencies = isDev ? localDependencies : remoteDependencies
+
 let package = Package(
     name: "Settings",
     platforms: [.iOS(.v13)],
@@ -12,15 +35,7 @@ let package = Package(
             name: "Settings",
             targets: ["Settings"]),
     ],
-    dependencies: [
-        .package(url: "https://github.com/Swinject/Swinject.git", from: "2.8.0"),
-        .package(url: "https://github.com/arman095095/Managers.git", branch: "develop"),
-        .package(url: "https://github.com/arman095095/Module.git", branch: "develop"),
-        .package(url: "https://github.com/arman095095/DesignSystem.git", branch: "develop"),
-        .package(url: "https://github.com/arman095095/AlertManager.git", branch: "develop"),
-        .package(url: "https://github.com/arman095095/Account.git", branch: "develop"),
-        .package(url: "https://github.com/arman095095/Profile.git", branch: "develop")
-    ],
+    dependencies: dependencies,
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
